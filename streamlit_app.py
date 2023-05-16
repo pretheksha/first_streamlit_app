a68772ee24a3e96b34778edd62880ee5fe2bc60a
pretheksha <133781905+pretheksha@users.noreply.github.com>
2023-05-16 10:05:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title("Pretheksha is a beautiful, confident and strong person")
 streamlit.header("Breakfast Menu")
-streamlit.body("Omega 3 and blueberry oatmeal")
+streamlit.trailer("Omega 3 and blueberry oatmeal")
