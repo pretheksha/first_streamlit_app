22607cfb57b67c13e5e010c88c9242089b80d9ea
pretheksha <133781905+pretheksha@users.noreply.github.com>
2023-05-16 10:26:47 -0400

    spelling typo for streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,5 @@ streamlit.text("🥑🍞 avacado Toast")
 
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlist.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
               
