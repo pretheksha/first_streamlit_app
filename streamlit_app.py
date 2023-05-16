9f56559d3552e6b82f1b5517d7e4d78cdf66a94d
pretheksha <133781905+pretheksha@users.noreply.github.com>
2023-05-16 10:11:35 -0400

    Added emojis to items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 
 streamlit.title("Pretheksha is a beautiful, confident and strong person")
 streamlit.header("Breakfast Menu")
-streamlit.caption("Omega 3 and blueberry oatmeal")
+streamlit.caption(" 🥣 Omega 3 and blueberry oatmeal")
+streamlit.text("🥗 Kale, Spinach and Rocket smoothie")
+streamlit.text(" 🐔 Hard Boiled Free Range eggs")
+streamlit.text("🥑🍞 avacado Toast")
+              
