3adda0241cd629f30f834e86bebe7b7dd659c97f
pretheksha <133781905+pretheksha@users.noreply.github.com>
2023-05-19 13:55:47 -0400

    display the fruit load list

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ streamlit.dataframe(fruityvice_normalized) #display the data in tabular format
 #snowflake connector code
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The food load list contains:")
 streamlit.text(my_data_row)
 
               
