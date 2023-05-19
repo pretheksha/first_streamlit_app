bdcfb295fa00a3ca5604108735d1cf721acdf4c0
pretheksha <133781905+pretheksha@users.noreply.github.com>
2023-05-19 14:00:23 -0400

    Fetching all the rows into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ streamlit.dataframe(fruityvice_normalized) #display the data in tabular format
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The food load list contains:")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
               
