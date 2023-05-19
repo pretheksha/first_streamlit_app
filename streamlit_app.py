394c8c4f3d2b3ce2c616de80e9f2e1426e88c4c7
pretheksha <133781905+pretheksha@users.noreply.github.com>
2023-05-19 13:39:57 -0400

    sf connector code

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #take the json version of the data and normalize it
 streamlit.dataframe(fruityvice_normalized) #display the data in tabular format
+#snowflake connector code
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
               
